d2a5e220465a8148070bacd8dc0b9dea222b48d7
mdipierro@cs.depaul.edu <mdipierro@cs.depaul.edu@8fb14a07-5544-0410-8dbc-e3a1454e9f17>
2008-03-27 07:22:28 +0000

    options.py renames options_std.py
    git-svn-id: http://web2py.googlecode.com/svn/trunk@127 8fb14a07-5544-0410-8dbc-e3a1454e9f17

--- options_std.py
+++ options_std.py
@@ -1,14 +1,14 @@
 import socket, os
 ip = '127.0.0.1'
 port = 8000
-password = 'admin'
+password = '<recycle>' ### <recycle> means use the previous password
 pid_filename = 'httpserver.pid'
 log_filename = 'httpserver.log'
-ssl_certificate = ''
-ssl_private_key = ''
+ssl_certificate = '' ### path to certificate file
+ssl_private_key = '' ### path to private key file
 numthreads = 10
 server_name = socket.gethostname()
 request_queue_size = 5
 timeout = 10
 shutdown_timeout = 5
-path = os.getcwd()
+folder = os.getcwd()
