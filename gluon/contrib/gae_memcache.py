47d9f344eb9082e64a20a8ab0cdc1b8b75d9ce85
mdipierro@cs.depaul.edu <mdipierro@cs.depaul.edu@8fb14a07-5544-0410-8dbc-e3a1454e9f17>
2008-07-29 11:28:01 +0000

    added gluon/contrib/gae_memcache (thanks Robin)
    git-svn-id: http://web2py.googlecode.com/svn/trunk@348 8fb14a07-5544-0410-8dbc-e3a1454e9f17

---
+++ gluon/contrib/gae_memcache.py
@@ -0,0 +1,27 @@
+"""
+Developed by Robin Bhattacharyya (memecache for GAE)
+
+from gluon.contrib.gae_memcache import MemcacheClient
+cache.ram=cache.disk=MemcacheClient(response)
+"""
+
+import time
+from google.appengine.api.memcache import Client
+
+class MemcacheClient(Client):
+    def __init__(self, request):
+        self.request=request
+        Client.__init__(self)
+    def __call__(self,key,f,time_expire=300):
+        key='%s/%s' % (self.request.application,key)
+        dt=time_expire
+        value=None
+        obj=self.get(key)
+        if obj and obj[0]>time.time()-dt:
+            value=obj[1]
+        elif f is None:
+            if obj: self.delete(key)
+        else:
+            value=f()
+            self.set(key,(time.time(),value))
+        return value
