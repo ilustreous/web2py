a6302c5414547d10b0c2cb419226515182ca5d18
mdipierro@cs.depaul.edu <mdipierro@cs.depaul.edu@8fb14a07-5544-0410-8dbc-e3a1454e9f17>
2008-08-19 18:55:28 +0000

    cgihandler sets path and cwd
    git-svn-id: http://web2py.googlecode.com/svn/trunk@378 8fb14a07-5544-0410-8dbc-e3a1454e9f17

--- cgihandler.py
+++ cgihandler.py
@@ -1,3 +1,9 @@
-import gluon.main, wsgiref.handlers
+import gluon.main
+import wsgiref.handlers
+import sys, os
+sys.path.insert(0,'')
+path=os.path.dirname(os.path.abspath(__file__))
+if not path in sys.path: sys.path.append(path)
+os.chdir(path)
 
 wsgiref.handlers.CGIHandler().run(gluon.main.wsgibase)
