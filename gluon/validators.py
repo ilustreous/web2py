e3c282628cea63f51bec8723e514c67cd8105924
mdipierro@cs.depaul.edu <mdipierro@cs.depaul.edu@8fb14a07-5544-0410-8dbc-e3a1454e9f17>
2008-12-31 01:01:34 +0000

    fixed bug with uuid, thanks Fran
    git-svn-id: http://web2py.googlecode.com/svn/trunk@604 8fb14a07-5544-0410-8dbc-e3a1454e9f17

--- gluon/validators.py
+++ gluon/validators.py
@@ -104,7 +104,7 @@ class IS_IN_SET(object):
             return [(k, k) for i, k in enumerate(self.theset)]
         return [(k, self.labels[i]) for i, k in enumerate(self.theset)]
     def __call__(self, value):
-        if self.multiple: values=re.compile("\w+").findall(str(value))
+        if self.multiple: values=re.compile("[^\s\|]+").findall(str(value))
         else: values=[value]
         failures=[x for x in values if not x in self.theset]
         if failures: return (value, self.error_message)
@@ -162,7 +162,7 @@ class IS_IN_DB(object):
         return [(k,self.labels[i]) for i,k in enumerate(self.theset)]
     def __call__(self,value):
         if self.multiple:
-            values=re.compile("\w+").findall(str(value))
+            values=re.compile("[^\s\|]+").findall(str(value))
             if not [x for x in values if not x in self.theset]:
                 return ('|%s|'%'|'.join(values),None)
         elif self.theset:
