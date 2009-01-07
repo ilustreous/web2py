541090296cd6dcd38bc2c0026272cc0feb804ef5
mdipierro@cs.depaul.edu <mdipierro@cs.depaul.edu@8fb14a07-5544-0410-8dbc-e3a1454e9f17>
2009-01-07 06:11:49 +0000

    new template.py, includes some of Denes' ideas
    git-svn-id: http://web2py.googlecode.com/svn/trunk@627 8fb14a07-5544-0410-8dbc-e3a1454e9f17

--- gluon/template.py
+++ gluon/template.py
@@ -5,20 +5,16 @@ License: GPL v2
 """
 
 import re, cgi, sys, os
-from restricted import *
 
-__all__=['reindent','parse_template']
+__all__=['reindent','parse','parse_template']
 
-re_write=re.compile('\{\{=(?P<value>.*?)\}\}',re.DOTALL)
-re_html=re.compile('\}\}.*?\{\{',re.DOTALL)
-#re_strings=re.compile('((?:""").*?(?:"""))|'+"((?:''').*?(?:'''))"+'((?:""").*?(?:"""))|'+"((?:''').*?(?:'''))"
-
-PY_STRING_LITERAL_RE= r'(?P<name>'+ \
-  r"[uU]?[rR]?(?:'''(?:[^']|'{1,2}(?!'))*''')|" +\
-              r"(?:'(?:[^'\\]|\\.)*')|" +\
-            r'(?:"""(?:[^"]|"{1,2}(?!"))*""")|'+ \
-              r'(?:"(?:[^"\\]|\\.)*"))'
-re_strings=re.compile(PY_STRING_LITERAL_RE,re.DOTALL)
+re_open=re.compile('\'(\'{2})?|\"(\"{2})?|\}\}',re.M)
+regexes={
+"'": re.compile("(?<!\\\\)(\\\\\\\\)*'",re.M),
+'"': re.compile('(?<!\\\\)(\\\\\\\\)*"',re.M),
+"'''": re.compile("'''",re.M),
+'"""': re.compile('"""',re.M),
+}
 
 re_include_nameless=re.compile('\{\{\s*include\s*\}\}')
 re_include=re.compile('\{\{\s*include\s+(?P<name>.+?)\s*\}\}',re.DOTALL)
@@ -30,6 +26,7 @@ def reindent(text):
     credit=k=0
     for raw_line in lines:
        line=raw_line.strip()
+       if line[:1]=='=': line='response.write(%s)' % line[1:]
        if line[:5]=='elif ' or line[:5]=='else:' or    \
             line[:7]=='except:' or line[:7]=='except ' or \
             line[:7]=='finally:':
@@ -49,6 +46,35 @@ def reindent(text):
     text='\n'.join(new_lines)
     return text
 
+def parse(text):
+    s,i,state='',0,0
+    while text:
+        if not state:  ### html
+	     i=text.find('{{')
+             if i<0: i=len(text)
+             s+='response.write(%s,False)\n' % repr(text[:i])
+             text=text[i+2:]
+             state=1
+        else:
+             m=re_open.search(text)             
+             if not m:
+                  s+='%s\n' % text
+                  break
+             state,i=m.group(),m.end()
+             if state=='}}':
+                  s+='%s\n' % text[:i-2]
+                  text=text[i:]
+                  state=0
+             else:
+                  s+=text[:i]
+                  text=text[i:]
+                  m=regexes[state].search(text)
+                  if not m: raise SyntaxError, "Unbalanced quotes"
+                  i=m.end()
+                  s+=text[:i].replace('\n','\\n')
+                  text=text[i:]
+    return reindent(s)
+
 def replace(regex,text,f,count=0):
     i=0
     output=[]
@@ -62,41 +88,42 @@ def replace(regex,text,f,count=0):
     return ''.join(output)
 
 def parse_template(filename,path='views/',cache='cache/',context=dict()):
+    import restricted
     filename=filename
     ##
     # read the template
     ##
-    try: data=open(os.path.join(path,filename),'rb').read()
-    except IOError: raise RestrictedError('Processing View '+filename,
-                                  '','Unable to find the file')
+    try: text=open(os.path.join(path,filename),'rb').read()
+    except IOError:
+        raise restricted.RestrictedError('Processing View '+filename,
+              '','Unable to find the file')
     # check whether it extends a layout
     while 1:
-        match=re_extend.search(data)
+        match=re_extend.search(text)
         if not match: break
         t=os.path.join(path,eval(match.group('name'),context))
         try: parent=open(t,'rb').read()
-        except IOError: raise RestrictedError('Processing View '+filename,data,'','Unable to open parent view file: '+t)
+        except IOError:
+            raise restricted.RestrictedError('Processing View '+filename,text,
+                  '','Unable to open parent view file: '+t)
         a,b=match.start(),match.end()
-        data=data[0:a]+replace(re_include_nameless,parent,lambda x: data[b:])
+        text=text[0:a]+replace(re_include_nameless,parent,lambda x: text[b:])
     ##
     # check whether it includes subtemplates
     ##
     while 1:
-        match=re_include.search(data)
+        match=re_include.search(text)
         if not match: break
         t=os.path.join(path,eval(match.group('name'),context))
         try: child=open(t,'rb').read()
-        except IOError: raise RestrictedError('Processing View '+filename,data,'','Unable to open included view file: '+t)
-        data=replace(re_include,data,lambda x: child,1)
-
-    ##
-    # now convert to a python expression
-    ##
-    text='}}%s{{'%re_write.sub('{{response.write(\g<value>)}}',data)
-    text=replace(re_html,text,lambda x: '\nresponse.write(%s,escape=False)\n'%repr(x[2:-2]))
-    text=replace(re_strings,text,lambda x: x.replace('\n','\\n'))
-    return reindent(text)
+        except IOError:
+            raise restricted.RestrictedError('Processing View '+filename,text,
+                  '','Unable to open included view file: '+t)
+        text=replace(re_include,text,lambda x: child,1)
+    try: return parse(text)
+    except SyntaxError:
+        raise restricted.RestrictedError('Processing View '+filename,text,
+              '','Unbalanced quotation')
 
 if __name__=='__main__':
     print parse_template(sys.argv[1],path='../applications/welcome/views/')
-
