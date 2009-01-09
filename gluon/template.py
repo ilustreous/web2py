3efcc3f5be7128d8cc1d7f23f0c4c582bec12704
mdipierro@cs.depaul.edu <mdipierro@cs.depaul.edu@8fb14a07-5544-0410-8dbc-e3a1454e9f17>
2009-01-09 03:48:12 +0000

    again reverted back to old template.py
    git-svn-id: http://web2py.googlecode.com/svn/trunk@638 8fb14a07-5544-0410-8dbc-e3a1454e9f17

--- gluon/template.py
+++ gluon/template.py
@@ -6,81 +6,47 @@ License: GPL v2
 
 import re, sys, os
 
-__all__=['reindent','parse','parse_template']
+__all__=['reindent','parse_template']
 
-### regex for indentation
-re_block=re.compile('^(elif |else:|except:|except |finally:).*$',re.DOTALL)
-re_unblock=re.compile('^(return|continue|break)(\s.*)?$',re.DOTALL)
-re_pass=re.compile('^pass(\s.*)?$',re.DOTALL)
-### regex for parsing {{...}}
-re_open=re.compile('#|\'(\'{2})?|\"(\"{2})?|\}\}',re.MULTILINE)
-re_nl=re.compile('\\\\\s*\\n\s*|(?<=,|\(|\[|\{)\s*(#.*?)?\n\\s*')
-regexes={
-'#': re.compile('\\n|\}\}'),
-"'": re.compile("(?<!\\\\)(\\\\\\\\)*'"),
-'"': re.compile('(?<!\\\\)(\\\\\\\\)*"'),
-"'''": re.compile("'''",re.MULTILINE),
-'"""': re.compile('"""',re.MULTILINE),
-}
-### regex for extend and include
+re_write=re.compile('\{\{=(?P<value>.*?)\}\}',re.DOTALL)
+re_html=re.compile('\}\}.*?\{\{',re.DOTALL)
+#re_strings=re.compile('((?:""").*?(?:"""))|'+"((?:''').*?(?:'''))"+'((?:""").*?(?:"""))|'+"((?:''').*?(?:'''))"
+
+PY_STRING_LITERAL_RE= r'(?P<name>'+ \
+  r"[uU]?[rR]?(?:'''(?:[^']|'{1,2}(?!'))*''')|" +\
+              r"(?:'(?:[^'\\]|\\.)*')|" +\
+            r'(?:"""(?:[^"]|"{1,2}(?!"))*""")|'+ \
+              r'(?:"(?:[^"\\]|\\.)*"))'
+re_strings=re.compile(PY_STRING_LITERAL_RE,re.DOTALL)
+
 re_include_nameless=re.compile('\{\{\s*include\s*\}\}')
 re_include=re.compile('\{\{\s*include\s+(?P<name>.+?)\s*\}\}',re.DOTALL)
 re_extend=re.compile('\{\{\s*extend\s+(?P<name>.+?)\s*\}\}',re.DOTALL)
 
 def reindent(text):
-    new_lines,credit,k=[],0,0
-    for raw_line in text.split('\n'):
+    lines=text.split('\n')
+    new_lines=[]
+    credit=k=0
+    for raw_line in lines:
        line=raw_line.strip()
-       if not line: continue
-       if line[0]=='=': line='response.write(%s)' % line[1:]
-       if re_block.match(line): k=k+credit-1
+       if line[:5]=='elif ' or line[:5]=='else:' or    \
+            line[:7]=='except:' or line[:7]=='except ' or \
+            line[:7]=='finally:':
+                k=k+credit-1
        if k<0: k=0
        new_lines.append('    '*k+line)
        credit=0
-       if re_pass.match(line): credit,k=0,k-1
-       if re_unblock.match(line): credit,k=1,k-1
-       if line[-1]==':' and line[0]!='#': k+=1
-    return '\n'.join(new_lines)
-
-def parse(text):
-    otext=text
-    s,i,state='',0,0
-    ### state==0 -> in html (exit at {{)
-    ### state==1 -> in code (exit at }})
-    while text:
-        if not state:  ### html
-	     i=text.find('{{')
-             if i<0: i=len(text)
-             s+='response.write(%s,False)\n' % repr(text[:i])
-             text=text[i+2:]
-             state=1
-        else:
-             m=re_open.search(text)             
-             if not m:
-                  s+='%s\n' % text # multiline statements
-                  break
-             key,i=m.group(),m.end()
-             if key=='}}': # end of code
-                  s+='%s\n' % text[:i-2]
-                  text=text[i:]
-                  state=0
-             else: # found start of a comment or string
-                  s+=text[:i]
-                  text=text[i:]
-                  m=regexes[key].search(text)
-                  if m:
-                      i=m.end()
-                      if text[i-2:i]=='}}': i-=2 # this or newline closes a comment
-                      if len(key)==1: s+=text[:i] # comments and single quotes
-                      else: s+=text[:i].replace('\n','\\n') # multi-line only
-                      text=text[i:]
-                  else:
-                      k=otext[:-len(text)].count('\n')+1
-                      raise SyntaxError, "Unmatched quotation in line %s" % k
-    print s
-    s=re_nl.sub('',s)
-    print s
-    return reindent(s)
+       if line=='pass' or line[:5]=='pass ':
+            credit=0
+            k-=1
+       if line=='return' or line[:7]=='return ' or \
+          line=='continue' or line[:9]=='continue ' or \
+          line=='break' or line[:6]=='break':
+            credit=1
+            k-=1
+       if line[-1:]==':' and line.strip()[:1]!='#': k+=1
+    text='\n'.join(new_lines)
+    return text
 
 def replace(regex,text,f,count=0):
     i=0
@@ -96,35 +62,41 @@ def replace(regex,text,f,count=0):
 
 def parse_template(filename,path='views/',cache='cache/',context=dict()):
     import restricted
-    ### read the template
-    try: text=open(os.path.join(path,filename),'rb').read()
-    except IOError:
-        raise restricted.RestrictedError('Processing View '+filename,
-              '','Unable to find the file')
-    ### check whether it extends a layout
+    ##
+    # read the template
+    ##
+    try: data=open(os.path.join(path,filename),'rb').read()
+    except IOError: raise restricted.RestrictedError('Processing View '+filename,
+                                  '','Unable to find the file')
+    # check whether it extends a layout
     while 1:
-        match=re_extend.search(text)
+        match=re_extend.search(data)
         if not match: break
         t=os.path.join(path,eval(match.group('name'),context))
         try: parent=open(t,'rb').read()
-        except IOError:
-            raise restricted.RestrictedError('Processing View '+filename,text,
-                  '','Unable to open parent view file: '+t)
+        except IOError: raise restricted.RestrictedError('Processing View '+filename,data,
+                        '','Unable to open parent view file: '+t)
         a,b=match.start(),match.end()
-        text=text[0:a]+replace(re_include_nameless,parent,lambda x: text[b:])
-    ### check whether it includes subtemplates
+        data=data[0:a]+replace(re_include_nameless,parent,lambda x: data[b:])
+    ##
+    # check whether it includes subtemplates
+    ##
     while 1:
-        match=re_include.search(text)
+        match=re_include.search(data)
         if not match: break
         t=os.path.join(path,eval(match.group('name'),context))
         try: child=open(t,'rb').read()
-        except IOError:
-            raise restricted.RestrictedError('Processing View '+filename,text,
-                  '','Unable to open included view file: '+t)
-        text=replace(re_include,text,lambda x: child,1)
-    try: return parse(text)
-    except SyntaxError, e:
-            raise restricted.RestrictedError('Processing View '+filename,text,'',e)
+        except IOError: raise restricted.RestrictedError('Processing View '+filename,data,
+                        '','Unable to open included view file: '+t)
+        data=replace(re_include,data,lambda x: child,1)
+
+    ##
+    # now convert to a python expression
+    ##
+    text='}}%s{{'%re_write.sub('{{response.write(\g<value>)}}',data)
+    text=replace(re_html,text,lambda x: '\nresponse.write(%s,escape=False)\n'%repr(x[2:-2]))
+    text=replace(re_strings,text,lambda x: x.replace('\n','\\n'))
+    return reindent(text)
 
 if __name__=='__main__':
     print parse_template(sys.argv[1],path='../applications/welcome/views/')
