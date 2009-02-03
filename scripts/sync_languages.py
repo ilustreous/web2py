506b01f36ab58eefcff807bbb23a6b14e31be8f3
mdipierro@cs.depaul.edu <mdipierro@cs.depaul.edu@8fb14a07-5544-0410-8dbc-e3a1454e9f17>
2009-02-03 20:39:02 +0000

    pep8 scripts/*
    git-svn-id: http://web2py.googlecode.com/svn/trunk@700 8fb14a07-5544-0410-8dbc-e3a1454e9f17

--- scripts/sync_languages.py
+++ scripts/sync_languages.py
@@ -1,28 +1,33 @@
-import sys, shutil, os
-sys.path.insert(0,'.')
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+import shutil
+import os
+sys.path.insert(0, '.')
 from gluon.languages import findT
 
-file=sys.argv[1]
-apps=sys.argv[2:]
-d={}
+file = sys.argv[1]
+apps = sys.argv[2:]
+d = {}
 for app in apps:
-    path='applications/%s/'%app
-    findT(path,file)
-    d.update(eval(open(os.path.join(path,'languages','%s.py' % file)).read()))
-path='applications/%s/'%apps[-1]
-file1=os.path.join(path,'languages','%s.py' % file)
-f=open(file1,'w')
+    path = 'applications/%s/' % app
+    findT(path, file)
+    d.update(eval(open(os.path.join(path, 'languages', '%s.py'
+              % file)).read()))
+path = 'applications/%s/' % apps[-1]
+file1 = os.path.join(path, 'languages', '%s.py' % file)
+f = open(file1, 'w')
 f.write('{\n')
-keys=d.keys()
+keys = d.keys()
 keys.sort()
 for key in keys:
-    f.write("%s:%s,\n" % (repr(key),repr(str(d[key]))))
+    f.write('%s:%s,\n' % (repr(key), repr(str(d[key]))))
 f.write('}\n')
 f.close()
 
-oapps=apps[:-1]
+oapps = apps[:-1]
 oapps.reverse()
 for app in oapps:
-    path2='applications/%s/'%app
-    file2=os.path.join(path2,'languages','%s.py' % file)
-    shutil.copyfile(file1,file2)
+    path2 = 'applications/%s/' % app
+    file2 = os.path.join(path2, 'languages', '%s.py' % file)
+    shutil.copyfile(file1, file2)
--- scripts/tickets2db.py
+++ scripts/tickets2db.py
@@ -1,29 +1,47 @@
-SLEEP_MINUTES=5
-DB_URI='sqlite://tickets.db'
-ALLOW_DUPLICATES=True
-import sys, os, time, stat, datetime, md5
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+SLEEP_MINUTES = 5
+DB_URI = 'sqlite://tickets.db'
+ALLOW_DUPLICATES = True
+import sys
+import os
+import time
+import stat
+import datetime
+import md5
 from gluon.restricted import RestrictedError
-path=os.path.join(request.folder,'errors')
-hashes={}
-db=SQLDB(DB_URI)
-db.define_table('ticket',
+path = os.path.join(request.folder, 'errors')
+hashes = {}
+db = SQLDB(DB_URI)
+db.define_table(
+    'ticket',
     SQLField('app'),
     SQLField('name'),
-    SQLField('date_saved','datetime'),
+    SQLField('date_saved', 'datetime'),
     SQLField('layer'),
-    SQLField('traceback','text'),
-    SQLField('code','text'))
+    SQLField('traceback', 'text'),
+    SQLField('code', 'text'),
+    )
 while 1:
-   for file in os.listdir(path):
-      filename=os.path.join(path,file)
-      if not ALLOW_DUPLICATES:
-          key=md5.new(open(filename,'r').read()).hexdigest()
-          if hashes.has_key(key): continue      
-          hashes[key]=1
-      e=RestrictedError()
-      e.load(filename)
-      t=datetime.datetime.fromtimestamp(os.stat(filename)[stat.ST_MTIME])
-      db.ticket.insert(app=request.application,date_saved=t,name=file,layer=e.layer,traceback=e.traceback,code=e.code)
-      os.unlink(filename)
-   db.commit()
-   time.sleep(SLEEP_MINUTES*60)
+    for file in os.listdir(path):
+        filename = os.path.join(path, file)
+        if not ALLOW_DUPLICATES:
+            key = md5.new(open(filename, 'r').read()).hexdigest()
+            if hashes.has_key(key):
+                continue
+            hashes[key] = 1
+        e = RestrictedError()
+        e.load(filename)
+        t = \
+            datetime.datetime.fromtimestamp(os.stat(filename)[stat.ST_MTIME])
+        db.ticket.insert(
+            app=request.application,
+            date_saved=t,
+            name=file,
+            layer=e.layer,
+            traceback=e.traceback,
+            code=e.code,
+            )
+        os.unlink(filename)
+    db.commit()
+    time.sleep(SLEEP_MINUTES * 60)
